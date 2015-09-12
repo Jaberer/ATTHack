9db0945e161ca3602be5c87ba906f6f80ddc23bc
Ac2zoom <ac2zoom@yahoo.com>
2015-09-12 16:47:28 -0700

    added myosdk to source files

--- WeelzAndroid/app/src/main/java/weelzandroid/cs/washington/edu/weelzandroid/MapsActivity.java
+++ WeelzAndroid/app/src/main/java/weelzandroid/cs/washington/edu/weelzandroid/MapsActivity.java
@@ -5,6 +5,7 @@ import android.graphics.Color;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
@@ -53,6 +54,12 @@ public class MapsActivity extends FragmentActivity implements
         hideTextOnLost();
         addListenerOnButton();
         selectedPin();
+        Hub hub = Hub.getInstance();
+        if (!hub.init(this)) {
+            Log.e(TAG, "Could not initialize the Hub.");
+            finish();
+            return;
+        }
     }
 
     public void addListenerOnButton() {
