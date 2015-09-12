e728932982508f44af275c1566c529a588418a93
Ac2zoom <ac2zoom@yahoo.com>
2015-09-12 01:35:34 -0700

    Node Modules

--- WeelzBackend/getPins.js
+++ WeelzBackend/getPins.js
@@ -1,7 +1,7 @@
 app.get('/getPins', function(req, res){
 
     // Find one document in our collection
-    db.collection('pins').find({loc:{$near:req.loc}}, function(err, doc) {
+    db.collection('pins').find({loc:{$near:req.query['loc']}}, function(err, doc) {
 
         if(err) throw err;
 
